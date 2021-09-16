a274b90d4ad9e1ebecd4f4bb6f65f68c888516a8
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-16 14:57:51 -0400

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About Dad Jokes </h1>
+    <p>This is a dad joke app tutorial</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head(){
+    return {
+      title: 'Daddy Jokes',
+      meta: [
+        {
+          hid: 'description', //unqiue idenfier
+          name: 'description', //Acutal meta tag
+          content: 'Best corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
